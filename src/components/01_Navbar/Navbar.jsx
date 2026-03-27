3c89dc70aa87f21af65d984ab9527e318c250d1e
Abhinav Kartik <abhinavkartik2026@gmail.com>
2026-03-28 00:00:56 +0530

    Navbar Section

--- src/components/01_Navbar/Navbar.jsx
+++ src/components/01_Navbar/Navbar.jsx
@@ -1,26 +1,35 @@
+import logo from "../../assets/Center_logo.png";
+
 function Navbar() {
   return (
-    <header className="bg-black text-white px-8 py-4">
-      <div className="max-w-7xl mx-auto flex justify-between items-center">
+    <header className="fixed top-0 w-full backdrop-blur bg-black/50 border-b border-gray-800">
 
-        <h1 className="text-2xl font-bold text-red-500">
-          FitZone Gym
-        </h1>
+      <div className="text-size-xl max-w-8xl mx-auto flex items-center justify-between px-8 py-5 text-white">
 
-        <nav className="flex gap-8">
-          <a href="#" className="hover:text-red-500">Home</a>
-          <a href="#" className="hover:text-red-500">About</a>
-          <a href="#" className="hover:text-red-500">Services</a>
-          <a href="#" className="hover:text-red-500">Contact</a>
+        {/* Logo */}
+        <div className="flex items-center gap-2">
+          <img src={logo} alt="logo" className="w-10 h-10" />
+          <h1 className="text-lg font-bold text-red-500">
+            FitZone Gym
+          </h1>
+        </div>
+
+        {/* Navigation */}
+        <nav className="flex gap-8 text-sm font-medium">
+          <a href="#home" className="hover:text-red-500 transition">Home</a>
+          <a href="#services" className="hover:text-red-500 transition">Programs</a>
+          <a href="#pricing" className="hover:text-red-500 transition">Pricing</a>
+          <a href="#features" className="hover:text-red-500 transition">About</a>
         </nav>
-        
-        <button className="bg-red-600 px-4 py-2 rounded-lg hover:bg-red-700">
+
+        {/* Button  */}
+        <button className="bg-red-600 px-5 py-2 rounded-full hover:bg-red-700 transition">
           Join Now
         </button>
 
       </div>
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
